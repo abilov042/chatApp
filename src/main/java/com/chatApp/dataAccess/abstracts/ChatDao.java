80b0ba7cd0d2b005112727a75361bff3b0667ef3
abilov042 <shamilabilov599@gmail.com>
2023-10-02 23:38:40 +0400

    created Room table

--- src/main/java/com/chatApp/dataAccess/abstracts/ChatDao.java
+++
@@ -1,7 +0,0 @@
-package com.chatApp.dataAccess.abstracts;
-
-import com.chatApp.entities.concretes.Chat;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatDao extends JpaRepository<Chat, Integer> {
-}
